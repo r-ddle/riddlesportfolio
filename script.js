dcd5e399f3951059f93281e3af020cb1221022e1
Riddle <155899302+r-ddle@users.noreply.github.com>
2024-05-27 15:09:02 +0530

    Minor Fix : Added LazyLoading function (progress)
    Update for Script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 import "animate.css";
+
+function lazyLoad() {
+    var lazyImages = document.querySelectorAll('[data-src]');
+    lazyImages.forEach(function(image) {
+      var imageSource = image.dataset.src;
+      image.src = imageSource;
+    });
+  }
+  
+  window.addEventListener('scroll', lazyLoad);
