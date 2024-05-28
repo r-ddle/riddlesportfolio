6d112c7823741ac6862fe269d2594d92a446f009
Riddle <155899302+r-ddle@users.noreply.github.com>
2024-05-28 14:51:54 +0530

    Update : Mobile Responsiveness
    - hid projects to not over load the side with miny projects
    - added load more button to veiw projects
    - site is now mobile responsive (hopefully)

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-import "animate.css";
+const loadMoreBtn = document.querySelector(".loadMore");
+const cards = document.querySelectorAll(".card");
+let numCardsRevealed = 4;
+
+// Make the first 4 cards active by default
+for (let i = 0; i < 4; i++) {
+  cards[i].classList.add("active");
+}
+
+loadMoreBtn.addEventListener("click", () => {
+  for (
+    let i = numCardsRevealed;
+    i < numCardsRevealed + 4 && i < cards.length;
+    i++
+  ) {
+    cards[i].classList.add("active");
+  }
+  numCardsRevealed += 4;
+  if (numCardsRevealed >= cards.length) {
+    loadMoreBtn.style.display = "none";
+  }
+});
 
 function lazyLoad() {
-    var lazyImages = document.querySelectorAll('[data-src]');
-    lazyImages.forEach(function(image) {
-      var imageSource = image.dataset.src;
-      image.src = imageSource;
-    });
-  }
-  
-  window.addEventListener('scroll', lazyLoad);
+  var lazyImages = document.querySelectorAll("[data-src]");
+  lazyImages.forEach(function (image) {
+    var imageSource = image.dataset.src;
+    image.src = imageSource;
+  });
+}
+
+window.addEventListener("scroll", lazyLoad);
